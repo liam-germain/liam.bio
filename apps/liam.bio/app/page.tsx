e2cffe409bf435466c278e8de1315ee7ebf8c8cf
Liam <liam@plootus.xyz>
2022-12-16 23:27:01 -0500

    [fix] maybe now.

--- apps/liam.bio/app/page.tsx
+++ apps/liam.bio/app/page.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function Page() {
   return (
     <div>
-      hello, I am liam germain
+      hello, I am liam germain. I hope this fixes the website
     </div>
   );
 }
